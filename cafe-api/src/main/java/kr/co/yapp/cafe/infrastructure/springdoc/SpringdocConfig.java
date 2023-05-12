987dd646780af1117ff28f6009aeaa6bd80f9406
Haeseong Jeon <seong0428@gmail.com>
2023-05-13 03:43:40 +0900

    config: Add springdoc

---
+++ cafe-api/src/main/java/kr/co/yapp/cafe/infrastructure/springdoc/SpringdocConfig.java
@@ -0,0 +1,9 @@
+package kr.co.yapp.cafe.infrastructure.springdoc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringdocConfig {
+}
