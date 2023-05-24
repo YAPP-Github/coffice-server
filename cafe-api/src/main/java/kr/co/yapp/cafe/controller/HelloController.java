77343152e0a62568dc7c60df94a9feb45b7a067a
Seungwoo Kim <sonicdx886@gmail.com>
2023-05-24 18:13:06 +0900

    config: api 배포 스크립트 추가

---
+++ cafe-api/src/main/java/kr/co/yapp/cafe/controller/HelloController.java
@@ -0,0 +1,14 @@
+package kr.co.yapp.cafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello";
+    }
+}
