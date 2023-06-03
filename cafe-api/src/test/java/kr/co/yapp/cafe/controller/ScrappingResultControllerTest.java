f4b0ac129ca69573dbc7507998873f87cc11a96a
Haeseong Jeon <seong0428@gmail.com>
2023-06-03 14:59:27 +0900

    refactor: request body 생성 방식 개선

--- cafe-api/src/test/java/kr/co/yapp/cafe/controller/ScrappingResultControllerTest.java
+++ cafe-api/src/test/java/kr/co/yapp/cafe/controller/ScrappingResultControllerTest.java
@@ -1,6 +1,7 @@
 package kr.co.yapp.cafe.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.yapp.cafe.controller.dto.ScrappingResultCreateRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,14 +24,28 @@ class ScrappingResultControllerTest {
 
     @Test
     void create() throws Exception {
+        // given
+        byte[] content = objectMapper.writeValueAsBytes(
+                createScrappingResultCreateRequest("test")
+        );
+        // when
         mockMvc.perform(
                         post("/api/v1/scrapping-results")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content("{\"name\": \"test\"}")
+                                .content(content)
                 )
+                // then
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.scrappingResultId").value(1L))
                 .andExpect(jsonPath("$.name").value("test"));
+    }
+
+    private ScrappingResultCreateRequest createScrappingResultCreateRequest(
+            String name
+    ) {
+        var scrappingResultCreateRequest = new ScrappingResultCreateRequest();
+        scrappingResultCreateRequest.setName(name);
+        return scrappingResultCreateRequest;
     }
 }
