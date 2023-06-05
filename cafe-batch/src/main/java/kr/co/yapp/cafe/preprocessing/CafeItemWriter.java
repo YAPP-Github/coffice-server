696b60c274d1bec72af46c92c996390a8782374e
Haeseong Jeon <seong0428@gmail.com>
2023-06-05 23:43:46 +0900

    feat: DB 저장 로직 추가

--- cafe-batch/src/main/java/kr/co/yapp/cafe/preprocessing/CafeItemWriter.java
+++ cafe-batch/src/main/java/kr/co/yapp/cafe/preprocessing/CafeItemWriter.java
@@ -1,14 +1,27 @@
 package kr.co.yapp.cafe.preprocessing;
 
+import kr.co.yapp.cafe.domain.scrapping.ScrappingRepository;
+import kr.co.yapp.cafe.domain.scrapping.ScrappingResult;
 import kr.co.yapp.cafe.domain.scrapping.ScrappingResultCreateVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Slf4j
 public class CafeItemWriter implements ItemWriter<ScrappingResultCreateVo> {
+    @Autowired
+    private ScrappingRepository scrappingRepository;
+
     @Override
     public void write(Chunk<? extends ScrappingResultCreateVo> chunk) throws Exception {
         log.info("{}", chunk);
+        List<ScrappingResult> scrappingResults = chunk.getItems()
+                .stream()
+                .map(ScrappingResult::from)
+                .toList();
+        scrappingRepository.saveAll(scrappingResults);
     }
 }
