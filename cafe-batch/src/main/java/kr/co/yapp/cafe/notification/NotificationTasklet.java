ca40dc763f15574d9d1594733f4ddd44f1954d9f
Haeseong Jeon <seong0428@gmail.com>
2023-06-13 01:42:42 +0900

    feat: 테스트 배치 추가

---
+++ cafe-batch/src/main/java/kr/co/yapp/cafe/notification/NotificationTasklet.java
@@ -0,0 +1,26 @@
+package kr.co.yapp.cafe.notification;
+
+import kr.co.yapp.cafe.notification.core.NotificationRequestVo;
+import kr.co.yapp.cafe.notification.core.NotificationResponseVo;
+import kr.co.yapp.cafe.notification.core.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Slf4j
+public class NotificationTasklet implements Tasklet {
+    @Autowired
+    private NotificationService notificationService;
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        NotificationRequestVo notificationRequestVo = NotificationRequestVo.of("title", "content", "fcmToken");
+        log.info("request: {}", notificationRequestVo);
+        NotificationResponseVo notificationResponseVo = notificationService.send(notificationRequestVo);
+        log.info("response: {}", notificationResponseVo);
+        return RepeatStatus.FINISHED;
+    }
+}
