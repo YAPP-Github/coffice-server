fb21451dcf636f0ddc993d409bcd34425cc7f0b0
Haeseong Jeon <seong0428@gmail.com>
2023-06-13 22:59:07 +0900

    feat: 테스트용 accessToken 검증 로직 추가

--- cafe-api/src/main/java/kr/co/yapp/cafe/ui/ResultCode.java
+++ cafe-api/src/main/java/kr/co/yapp/cafe/ui/ResultCode.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 public enum ResultCode {
     SUCCESS("성공"),
     FAILURE("실패"),
+    UNAUTHORIZED("인증이 필요한 요청입니다."),
+    FORBIDDEN("권한이 부족합니다."),
+    NOT_FOUND("대상을 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR("서버 오류가 발생했습니다."),
     ;
 
     private final String message;
