b86b42d58431d7bdd3c44bcd7c1683e5ab958d42
Haeseong Jeon <seong0428@gmail.com>
2023-06-13 23:44:03 +0900

    fix: 문서에 잘못된 서버 주소 나오는 현상 해결

--- cafe-api/src/main/java/kr/co/yapp/cafe/infrastructure/springdoc/SpringdocConfig.java
+++ cafe-api/src/main/java/kr/co/yapp/cafe/infrastructure/springdoc/SpringdocConfig.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition
+@OpenAPIDefinition(
+        servers = @Server(url = "/")
+)
 @SecurityScheme(
         name = SpringdocConfig.SECURITY_SCHEME_NAME,
         bearerFormat = "JWT",
