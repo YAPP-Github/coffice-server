01f7146eabd59079712df307911e1bd29dd53b04
Haeseong Jeon <seong0428@gmail.com>
2023-06-16 21:40:54 +0900

    feat: 로그 추가

--- coffice-admin/src/main/java/kr/co/yapp/_22nd/coffice/infrastructure/security/CofficeAdminOidcUserService.java
+++ coffice-admin/src/main/java/kr/co/yapp/_22nd/coffice/infrastructure/security/CofficeAdminOidcUserService.java
@@ -2,12 +2,14 @@ package kr.co.yapp._22nd.coffice.infrastructure.security;
 
 import kr.co.yapp._22nd.coffice.domain.AdminMemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
+@Slf4j
 @RequiredArgsConstructor
 public class CofficeAdminOidcUserService implements OAuth2UserService<OidcUserRequest, OidcUser> {
     private final OidcUserService oidcUserService = new OidcUserService();
@@ -16,13 +18,15 @@ public class CofficeAdminOidcUserService implements OAuth2UserService<OidcUserRe
     @Override
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
         var loadedUser = oidcUserService.loadUser(userRequest);
-        if (!isInternalUser(loadedUser)) {
+        if (!isAdminMember(loadedUser)) {
+            //noinspection DataFlowIssue
+            log.warn("접근 권한이 없습니다. email: {}", String.valueOf(loadedUser.getAttribute("email")));
             throw new OAuth2AuthenticationException("접근 권한이 없습니다. email: " + loadedUser.getAttribute("email"));
         }
         return loadedUser;
     }
 
-    private boolean isInternalUser(OidcUser oidcUser) {
+    private boolean isAdminMember(OidcUser oidcUser) {
         String email = oidcUser.getAttribute("email");
         if (email == null) {
             return false;
