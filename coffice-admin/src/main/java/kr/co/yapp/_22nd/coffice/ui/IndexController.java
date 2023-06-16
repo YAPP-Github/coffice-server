3bedfc163322f9351b8971d1af20380d41feca69
전해성 <seong0428@gmail.com>
2023-06-16 23:13:11 +0900

    feat: 장소 목록, 상세 조회 (#55)

--- coffice-admin/src/main/java/kr/co/yapp/_22nd/coffice/ui/IndexController.java
+++ coffice-admin/src/main/java/kr/co/yapp/_22nd/coffice/ui/IndexController.java
@@ -1,4 +1,4 @@
-package kr.co.yapp._22nd.coffice.controller;
+package kr.co.yapp._22nd.coffice.ui;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
