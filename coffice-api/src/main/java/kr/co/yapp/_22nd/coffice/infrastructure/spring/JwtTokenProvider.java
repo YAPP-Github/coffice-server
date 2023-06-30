b1843c26b20303ca7852c97df8630903eae9e8f1
Seungwoo Kim <sonicdx886@gmail.com>
2023-06-30 16:51:00 +0900

    fix : 토큰 만료 기한 private 으로 수정

--- coffice-api/src/main/java/kr/co/yapp/_22nd/coffice/infrastructure/spring/JwtTokenProvider.java
+++ coffice-api/src/main/java/kr/co/yapp/_22nd/coffice/infrastructure/spring/JwtTokenProvider.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,9 +12,8 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
 @Component
-@RequiredArgsConstructor
 public class JwtTokenProvider {
-    static int EXPIRATION_DURATION_IN_YEAR = 100;
+    private static final int EXPIRATION_DURATION_IN_YEAR = 100;
     private final Algorithm algorithm;
 
     @Autowired
