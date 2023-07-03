01b8e32d5c3cfc704cd0ec54cbc431c3b9da27cf
전해성 <seong0428@gmail.com>
2023-07-04 01:35:08 +0900

    feat: data 가 배열이면 page 필드 항상 추가 (#91)

--- coffice-api/src/main/java/kr/co/yapp/_22nd/coffice/ui/PageResponse.java
+++ coffice-api/src/main/java/kr/co/yapp/_22nd/coffice/ui/PageResponse.java
@@ -1,9 +1,19 @@
 package kr.co.yapp._22nd.coffice.ui;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Slice;
 
 public record PageResponse(boolean hasNext) {
+
+    public static PageResponse unpaged() {
+        return new PageResponse(false);
+    }
+
     public static PageResponse from(Slice<?> slice) {
         return new PageResponse(slice.hasNext());
+    }
+
+    public static PageResponse from(Page<?> page) {
+        return new PageResponse(page.hasNext());
     }
 }
