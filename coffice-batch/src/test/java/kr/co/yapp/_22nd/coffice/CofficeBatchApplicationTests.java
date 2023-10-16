f71d4d0ffd880850cc4d0adc21516d77cb0b7195
Seungwoo Kim <sonicdx886@gmail.com>
2023-10-17 01:52:40 +0900

    fix: 빌드 에러 해결

--- coffice-batch/src/test/java/kr/co/yapp/_22nd/coffice/CofficeBatchApplicationTests.java
+++ coffice-batch/src/test/java/kr/co/yapp/_22nd/coffice/CofficeBatchApplicationTests.java
@@ -3,7 +3,7 @@ package kr.co.yapp._22nd.coffice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CofficeBatchApplicationTests {
     @Test
     void contextLoads() {
