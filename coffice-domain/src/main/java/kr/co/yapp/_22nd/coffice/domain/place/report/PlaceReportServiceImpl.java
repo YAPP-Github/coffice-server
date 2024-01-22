94d836f98a6c47b0cf47eaa58f1e971531fce934
Seungwoo Kim <sonicdx886@gmail.com>
2024-01-22 22:52:28 +0900

    feat: admin 제보하기 조회 기능

--- coffice-domain/src/main/java/kr/co/yapp/_22nd/coffice/domain/place/report/PlaceReportServiceImpl.java
+++ coffice-domain/src/main/java/kr/co/yapp/_22nd/coffice/domain/place/report/PlaceReportServiceImpl.java
@@ -3,6 +3,8 @@ package kr.co.yapp._22nd.coffice.domain.place.report;
 import kr.co.yapp._22nd.coffice.domain.member.Member;
 import kr.co.yapp._22nd.coffice.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,5 +23,17 @@ public class PlaceReportServiceImpl implements PlaceReportService{
         Member member = memberRepository.getReferenceById(memberId);
         PlaceReport placeReport = PlaceReport.of(member, placeReportCreateVo);
         return placeReportRepository.save(placeReport);
+    }
+
+    @Override
+    public Page<PlaceReport> findAll(Pageable pageable) {
+        return placeReportRepository.findAll(pageable);
+    }
+
+    /* TODO: Exception 정의 */
+    @Override
+    public PlaceReport getPlaceReport(Long placeReportId) {
+        return placeReportRepository.findById(placeReportId)
+                .orElseThrow(RuntimeException::new);
     }
 }
