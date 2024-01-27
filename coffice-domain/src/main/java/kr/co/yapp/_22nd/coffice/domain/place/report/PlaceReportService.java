655fb552c32d97632fc41ec67d796457abeb7e91
Seungwoo Kim <48646015+seungwooKim99@users.noreply.github.com>
2024-01-27 18:00:20 +0900

    feat: admin 제보하기 조회 기능 (#125)
    * feat: admin 제보하기 조회 기능
    
    * fix: sonarcloud 이슈 해결

--- coffice-domain/src/main/java/kr/co/yapp/_22nd/coffice/domain/place/report/PlaceReportService.java
+++ coffice-domain/src/main/java/kr/co/yapp/_22nd/coffice/domain/place/report/PlaceReportService.java
@@ -1,5 +1,10 @@
 package kr.co.yapp._22nd.coffice.domain.place.report;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PlaceReportService {
     PlaceReport create(Long memberId, PlaceReportCreateVo placeReportCreateVo);
+    Page<PlaceReport> findAll(Pageable pageable);
+    PlaceReport getPlaceReport(Long placeReportId);
 }
